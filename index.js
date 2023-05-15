c94ba8de74ab7633a5f5f012ba494d300dfc64ff
PearlPerfect <perfectpearl2030@gmail.com>
2023-05-15 16:12:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 console.log("My name is Favour Emmanuel, I am a web developer learning javascript at FutureLabs");
 let name = "Favour";
 console.log(`Hello ${name} how are you`)
+console.log("Git is kinda interesting, but na money i dey find")
